a16a4566369f63104bf098024df25912068bdd21
ossnms <global.ossnms@gmail.com>
2018-12-06 13:40:23 +0000

    Update version to 2.3.2-ossnms-002

--- src/org/jacorb/util/Version.java
+++ src/org/jacorb/util/Version.java
@@ -28,7 +28,7 @@ package org.jacorb.util;
  */
 public final class Version
 {
-    public static final String version = "2.3.2-ossnms-001";
+    public static final String version = "2.3.2-ossnms-002";
     public static final String date = "25-Nov-2015";
     public static final String longVersion = version + ", " + date;
     public static final String yearString = "1997-2015";
